02771d2c8c4c06e74a2816ac17c2074bb18835c9
maui <maui@devf.mx>
2019-07-05 20:27:22 -0500

    agregar dotenv para manejar la conexion a bd mediante variables de entorno

--- database/index.js
+++ database/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const db_uri = 'mongodb+srv://maui:abc123def@testing-cluster-efwi5.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(
-    db_uri, 
+    process.env.DB_URI, 
     { useNewUrlParser: true },
     () => console.log(`conexión exitosa con bd 🐢`));
